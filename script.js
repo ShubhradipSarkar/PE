7d86e3f1024e15cef48da0a6b3c8b3445b0b5c2c
shubhradip <shubhradipsarkar@gmail.com>
2026-05-27 21:45:21 +0530

    o

---
+++ script.js
@@ -0,0 +1,47 @@
+window.addEventListener("load", () => {
+  document.getElementById("loader").style.display = "none";
+});
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const revealTop = el.getBoundingClientRect().top;
+
+    if(revealTop < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY > 400) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+});
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// OPTIONAL WHATSAPP FORM REDIRECT
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", () => {
+  alert("Your enquiry has been submitted successfully.");
+});
